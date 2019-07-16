79aeceb42a6d9bb2e731c46669e033cf69bd9d03
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-15 23:19:11 -0700

    MVP num of islands

--- 200-Number-of-Islands.js
+++ 200-Number-of-Islands.js
@@ -23,3 +23,67 @@ Input:
 
 Output: 3
 */
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    var count = 0;
+    
+    for (var row = 0; row < grid.length; row++) {
+        for (var col = 0; col < grid[0].length; col++) {
+            
+            if (grid[row][col] === '1') {
+                checkConnection(grid,row,col);
+                count++;
+            }
+         
+        }
+    }
+    
+    return count;
+    
+};
+
+var checkConnection = function (grid,row,col) {
+    grid[row][col] = 'X';
+    
+    //check left
+    if(col > 0) {
+        if (grid[row][col-1] === '1') {
+            grid[row][col-1] === 'X';
+            checkConnection(grid,row,col-1);
+        }
+     
+    }
+    
+    //check right
+    if (col < grid[0].length-1) {
+        if (grid[row][col+1] ==='1'){
+            grid[row][col+1] === 'X';
+            checkConnection(grid,row,col+1);
+        }
+     
+    }
+    //check top
+    
+    if (row > 0) {
+        if(grid[row-1][col]==='1'){
+            grid[row-1][col] === 'X';
+            checkConnection(grid,row-1,col);
+        }
+   
+    }
+    
+    //check bottom 
+    if (row < grid.length -1) {
+        if(grid[row+1][col]==='1'){
+            grid[row+1][col] === 'X';
+            checkConnection(grid,row+1,col);
+        }
+   
+    }
+    return;
+    
+    
+}
