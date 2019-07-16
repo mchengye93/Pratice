27c43daa85658ebed7ecb00a28c05a20cfff725f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-15 21:07:26 -0700

    MVP sort color

--- Top 100/75-Sort-Colors.js
+++ Top 100/75-Sort-Colors.js
@@ -19,3 +19,34 @@ First, iterate the array counting number of 0's, 1's, and 2's, then overwrite
 array with total number of 0's, then 1's and followed by 2's.
 Could you come up with a one-pass algorithm using only constant space?
 */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    var count = {};
+    
+    
+    for (var i= 0; i < nums.length; i++) {
+        if (count[nums[i]] === undefined) {
+            count[nums[i]] = 1;
+        } else {
+            count[nums[i]] = count[nums[i]] + 1;
+        }
+    }
+    
+    var n = 0;
+    while (n < nums.length) {
+        for (var i = 0; i < 3; i++) {
+            if(count[i] !== undefined){
+               var counts = count[i]; 
+
+               for (var x = 0; x < counts; x++){
+                   nums[n] = i;
+                   n++;
+
+               }
+
+            }
+        }
+    }
