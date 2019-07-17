b7d8a8774b44d70dc6085a9dc2841b794b3e1bf5
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-16 22:19:46 -0700

    MVP move zeroes

--- Top 100 Questions/283-Move-Zeroes.js
+++ Top 100 Questions/283-Move-Zeroes.js
@@ -11,3 +11,26 @@ Note:
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
 */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    var count = 0;
+    var x = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[x] = nums[i];
+            x++;
+            
+        } else {
+            count = count + 1;
+        }
+    }
+    for (var i = nums.length-count; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+    return nums;
+      
+  
+};
