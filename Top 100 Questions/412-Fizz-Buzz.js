7515ac0afc9e10e435cbb5634de754ecb6c3319f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-16 21:50:27 -0700

    MVP fizzbuzz problem

--- Top 100 Questions/412-Fizz-Buzz.js
+++ Top 100 Questions/412-Fizz-Buzz.js
@@ -27,3 +27,26 @@ Return:
     "14",
     "FizzBuzz"
 ]*/
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    var result = [];
+    
+    for (var i = 1; i <=n ; i++) {
+        if (i %15 === 0 ) {
+            result.push('FizzBuzz');
+        } 
+        else if (i % 5 === 0) {
+            result.push('Buzz');
+        } 
+        else if  (i%3 ===0) {
+            result.push('Fizz');
+        }
+        else {
+            result.push(i+'');
+        }
+    }
+    return result;
+};
