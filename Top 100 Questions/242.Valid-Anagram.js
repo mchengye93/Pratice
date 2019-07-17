67b67a37b37bf159fa79b2125696c0b2a105af82
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-16 22:37:36 -0700

    MVP anagram

--- Top 100 Questions/242.Valid-Anagram.js
+++ Top 100 Questions/242.Valid-Anagram.js
@@ -17,3 +17,33 @@ Follow up:
 What if the inputs contain unicode characters? How would you adapt your 
 solution to such case?
 */
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    var letterCount = {};
+    for (var i = 0; i < s.length; i++) {
+       if(letterCount[s[i]] === undefined){
+          letterCount[s[i]] = 1; 
+       } else {
+           letterCount[s[i]] = 1 + letterCount[s[i]]; 
+       }
+    }
+    
+    for (var i = 0; i < t.length; i++) {
+        if (letterCount[t[i]] === undefined){
+            return false;
+        } else {
+            letterCount[t[i]] = letterCount[t[i]]-1;
+            if (letterCount[t[i]] < 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
