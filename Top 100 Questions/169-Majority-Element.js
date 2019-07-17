6ceed0917bfaa8d36bb0f9e3dc4437b2718ba137
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-16 22:30:43 -0700

    MVP majority element

--- Top 100 Questions/169-Majority-Element.js
+++ Top 100 Questions/169-Majority-Element.js
@@ -14,3 +14,23 @@ Example 2:
 Input: [2,2,1,1,1,2,2]
 Output: 2
 */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    var count = {};
+    for (var i = 0; i < nums.length;i++) {
+        if (count[nums[i]] === undefined){
+            count[nums[i]] = 1;
+        }
+        else {
+            count[nums[i]] = count[nums[i]]+1;
+        }
+    }
+    for (var keys in count) {
+        if (count[keys] >= Math.min(nums.length/2)) {
+            return keys;
+        }
+    }
+};
