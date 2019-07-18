cce3187e54a6da9d9de96ef5beabef7e54d5c280
Marbo Cheng Ye <marbochengye@gmail.com>
2019-07-17 19:44:32 -0700

    MVP reverse linkedlist

--- Top 100 Questions/206-Reverse-Linked-List.js
+++ Top 100 Questions/206-Reverse-Linked-List.js
@@ -10,3 +10,27 @@ Follow up:
 A linked list can be reversed either iteratively or recursively.
  Could you implement both?
  */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    var prev = null;
+    var current = head;
+    
+    while(current!== null) {
+        var temp = current.next;
+        current.next = prev;
+        prev = current;
+        current = temp;
+    }
+    return prev;
+    
+};
