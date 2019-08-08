ba76ce8589ff21e8f157374bae3811aa61b64015
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-07 20:32:24 -0700

    Hashmap query prompt part 2

--- CodeSignal/HashMapFunction.js
+++ CodeSignal/HashMapFunction.js
@@ -25,4 +25,30 @@ The hashmap looks like this after each query:
 4 query: {2: 4, 3: 5}
 5 query: answer is 5
 
+The result of the last get query for 3 is 5 in the resulting hashmap.
+
+For queryType = ["insert", "addToValue", "get", "insert", "addToKey", "addToValue", "get"] and query = [[1, 2], [2], [1], [2, 3], [1], [-1], [3]], the output should be hashMap(queryType, query) = 6.
+The hashmap looks like this after each query:
+1 query: {1: 2}
+2 query: {1: 4}
+3 query: answer is 4
+4 query: {1: 4, 2: 3}
+5 query: {2: 4, 3: 3}
+6 query: {2: 3, 3: 2}
+7 query: answer is 2
+The sum of the results for all the get queries is equal to 4 + 2 = 6.
+Input/Output
+[execution time limit] 4 seconds (js)
+[input] array.string queryType
+Array of query types. It is guaranteed that each queryType[i] is either "addToKey", "addToValue", "get", or "insert".
+Guaranteed constraints:
+1 ≤ queryType.length ≤ 105.
+[input] array.array.integer query
+Array of queries, where each query is represented either by two numbers for insertquery or by one number for other queries. It is guaranteed that during all queries all keys and values are in the range [-109, 109].
+Guaranteed constraints:
+query.length = queryType.length,
+1 ≤ query[i].length ≤ 2.
+[output] integer64
+The sum of the results for all get queries.
+
 */
