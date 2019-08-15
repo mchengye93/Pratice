68fcd89ef40ecee1329fd661a4da1eafecd9acbb
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-14 22:26:32 -0700

    Comment on problem

--- InterviewQuestions/longestStreak.js
+++ InterviewQuestions/longestStreak.js
@@ -1,4 +1,4 @@
-/*
+/* Noom - Frontend problem
 Given an array filled with daily calories budget and calorie intake for breakfast, lunch, and dinner.
 Return the longest streak of daily calorie intake of less than calories budget.
 
