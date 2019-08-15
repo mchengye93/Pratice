4de8915373e773458b0052713c49e5bd4a677576
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-14 22:25:12 -0700

    Longest streak problem

---
+++ InterviewQuestions/longestStreak.js
@@ -0,0 +1,5 @@
+/*
+Given an array filled with daily calories budget and calorie intake for breakfast, lunch, and dinner.
+Return the longest streak of daily calorie intake of less than calories budget.
+
+*/
