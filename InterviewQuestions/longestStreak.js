446e28c19650c85abf1f46cf0358945140e97eeb
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-14 22:26:12 -0700

    MVP longest Streak problem

--- InterviewQuestions/longestStreak.js
+++ InterviewQuestions/longestStreak.js
@@ -3,3 +3,28 @@ Given an array filled with daily calories budget and calorie intake for breakfas
 Return the longest streak of daily calorie intake of less than calories budget.
 
 */
+var longestStreak = (inputsArr) => {
+let inputsArr = input.split('\n');
+let longestStreak = 0;
+let currentStreak = 0;
+
+    for (let i = 0; i < inputsArr.length; i++) {
+
+        let input = inputsArr[i].split(',');
+        let calorieBudget = parseInt(input[0]);
+        let calorieIntake = parseInt(input[1])+parseInt(input[2])+parseInt(input[3]);
+        if (calorieIntake <= calorieBudget) {
+            currentStreak++;
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+            }
+        } else {
+            currentStreak = 0;
+        }
+        
+
+        
+    }
+    console.log(longestStreak);
+
+}
