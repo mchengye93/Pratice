f7a7d33d4436b53c3a5fb546dc2c3121679db8cd
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-05 22:30:07 -0700

    MVP merge sortedArr problem

--- MostFrequent/Easy/88.MergeSortedArr.js
+++ MostFrequent/Easy/88.MergeSortedArr.js
@@ -15,3 +15,42 @@ nums2 = [2,5,6],       n = 3
 
 Output: [1,2,2,3,5,6]
 */
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let nums1Copy = nums1.slice();
+    
+    let i = 0;
+    let x = 0;
+    let l = 0;
+    while( i < m && x < n) {
+        
+        if (nums1Copy[i] < nums2[x] ) {
+            nums1[l] = nums1Copy[i];
+            i++;
+            l++;
+        } else {
+            nums1[l] = nums2[x];
+            x++;
+            l++;
+        }
+    }
+    
+    while(i < m) {
+        nums1[l] = nums1Copy[i];
+        i++;
+        l++;
+    }
+    
+    while(x < n) {
+        nums1[l] = nums2[x];
+        x++;
+        l++;
+    }
+    
+};
