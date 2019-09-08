8fbd3e04f15327c481e13496de6933feb485aa2b
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-07 20:44:51 -0700

    valid parenthesis MVP

--- Interview/Twilio/20.ValidParenthesis.js
+++ Interview/Twilio/20.ValidParenthesis.js
@@ -29,3 +29,39 @@ Example 5:
 Input: "{[]}"
 Output: true
 */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if (s.length%2 !==0) {
+        return false;
+    }
+    let arr = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        console.log(arr);
+        if (s[i] === '(' ||s[i] === '{' || s[i] ==='[') {
+            arr.push(s[i]);
+        } else {
+            //get last element from array
+            let currentVal = arr.pop();
+            if (s[i] === ')' & currentVal !== '(') {
+                return false;
+            }
+            
+            if (s[i] === ']' & currentVal !== '[') {
+                return false;
+            }
+            
+            if (s[i] === '}' & currentVal !== '{') {
+                return false;
+            }
+            
+        }
+    }
+    if (arr.length === 0) {
+        return true;
+    }
+    return false;
+};
