531069db317a53455df6c4554c5dbef5d5c99dd5
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-07 20:36:38 -0700

    reverse int attempt

--- Interview/Twilio/7.ReverseInt.js
+++ Interview/Twilio/7.ReverseInt.js
@@ -18,3 +18,11 @@ Assume we are dealing with an environment which could only store integers within
 the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, 
 assume that your function returns 0 when the reversed integer overflows.
 */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(n) {
+    n = n +'';
+    return n.split('').reverse().join('');
+};
