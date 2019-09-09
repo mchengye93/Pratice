dc9686dd14ce3a89b72608e738e22a7c4816bc8d
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-08 21:14:19 -0700

    phone problem

--- Interview/Twilio/vanityNum.js
+++ Interview/Twilio/vanityNum.js
@@ -1,37 +1,4 @@
-'use strict';
 
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-
-
-/*
- * Complete the 'vanity' function below.
- *
- * The function is expected to return a STRING_ARRAY.
- * The function accepts following parameters:
- *  1. STRING_ARRAY codes
- *  2. STRING_ARRAY numbers
- */
 
 function vanity(codes, numbers) {
     let numToLetter = {
@@ -96,4 +63,3 @@ function vanity(codes, numbers) {
 
 }
 
-function main() {
