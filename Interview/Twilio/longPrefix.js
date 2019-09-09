6ef4eae079f5e2d1bb146d28cbc0f4c9f06b379f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-08 21:14:44 -0700

    prefix problem

--- Interview/Twilio/longPrefix.js
+++ Interview/Twilio/longPrefix.js
@@ -1,37 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-
-
-/*
- * Complete the 'match' function below.
- *
- * The function is expected to return a STRING_ARRAY.
- * The function accepts following parameters:
- *  1. STRING_ARRAY prefixes
- *  2. STRING_ARRAY numbers
- */
 
 function match(prefixes, numbers) {
    
@@ -54,4 +20,3 @@ function match(prefixes, numbers) {
     return prefixResults;
 }
 
-function main() {
