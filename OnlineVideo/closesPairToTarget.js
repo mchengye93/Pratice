f36a14d2764e49e11773572ec93d3f59dadc0876
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 15:35:38 -0700

    Pseudo code to problem

--- OnlineVideo/closesPairToTarget.js
+++ OnlineVideo/closesPairToTarget.js
@@ -1,2 +1,14 @@
 /* Given two arrays find pairs closes to target
 */
+/*
+I: Two arrays of intergets and target integer
+O: Pairs closest to target
+C: None
+E: None
+*/
+/* Pseudocode
+//Pair values from two arrays and find difference to target
+//Save pair into an object storing difference as key and pairs as value
+//Find minium key values and return value which should be a list of pairs with 
+min difference to target
+*/
