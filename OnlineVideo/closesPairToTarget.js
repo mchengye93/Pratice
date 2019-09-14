8a7fa131312ae4baef2d088b336ce25c5f10ab50
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 15:43:15 -0700

    Code written code for minpairs

--- OnlineVideo/closesPairToTarget.js
+++ OnlineVideo/closesPairToTarget.js
@@ -14,5 +14,17 @@ min difference to target
 */
 
 function pairsToTarget(arr1, arr2, target) {
+    let diffPairs = {};
     
+    for (let i = 0; i < arr1.length; i++) {
+        for (let x = 0; x < arr2.length; x++) {
+            let pair = [arr1[i],arr2[x]];
+            let difference = Math.abs(target - (arr1[i] + arr2[x]));
+        }
+    }
+    //Pair values from two arrays and find difference to target
+//Save pair into an object storing difference as key and pairs as value
+//Find minium key values and return value which should be a list of pairs with 
+//min difference to target
+
 }
