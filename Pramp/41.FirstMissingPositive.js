522b659c0843a33a5f0e932c829b88162ef6d597
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-15 00:06:46 -0700

    MVP firstMissingPositive

--- Pramp/41.FirstMissingPositive.js
+++ Pramp/41.FirstMissingPositive.js
@@ -17,3 +17,25 @@ Note:
 
 Your algorithm should run in O(n) time and uses constant extra space.
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    if (nums.length === 0) {
+        return 1;
+    }
+    let numHashMap = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        numHashMap[nums[i]] = 1;
+    }
+    
+    for (let i = 1; i <= nums.length+1; i++) {
+        if (numHashMap[i] === undefined) {
+            return i;
+        }
+    }
+    
+};
