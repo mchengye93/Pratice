23d60838473b016a09911441190beef11b6f4306
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 21:17:41 -0700

    MVP first recurring letter

--- OnlineVideo/firstRecurringChar.js
+++ OnlineVideo/firstRecurringChar.js
@@ -5,4 +5,14 @@ Given a string return the first recurring character
 function firstRecurringChar(string) {
     //Go through string and save onto a hashMap
     //If already exist in hashMap return the character
+    let charMap = {};
+
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i];
+        if (charMap[letter]) {
+            return letter;
+        } else{
+            charMap[letter] = 1;
+        }
+    }
 }
