4f805b29ed4602a4a3544739780ddb6b82b3fb27
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 21:00:18 -0700

    MVP multToTarget

--- OnlineVideo/findTwoIntegersThatMultiplyToTarget.js
+++ OnlineVideo/findTwoIntegersThatMultiplyToTarget.js
@@ -5,6 +5,24 @@ function multToTarget(array, target) {
     //Go through array once and check if you can find pair to multiply to target
     //To speed up store process store numbers that is divisible by target
     //Then go through each and see if divisor can be found
+    let divisibleNums = {};
+    for (let i = 0; i < array.length; i++) {
+        let num = array[i];
+        if ( (target% num)  === 0) {
+            divisibleNums[num] = num;
+        }
+    }
+    //check to see if you can give the other divisor
+    for (let key in divisibleNums) {
+        let multiple = divisibleNums[key];
+        let multNum = target/multiple;
 
+         //if found return pair
+        if (divisibleNums[multNum]) {
+            return [multNum, key];
+        }
+    }
+    return -1;
+   
 
 }
