438e7a93d46406aa574167c81e7383647f85908a
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 21:29:52 -0700

    Pseudo for firstNonRecurringChar

--- OnlineVideo/firstNonRecurringChar.js
+++ OnlineVideo/firstNonRecurringChar.js
@@ -1,3 +1,10 @@
 /*
 Given a string return first nonrecurring character
 */
+function firstNonRecurringChar(string) {
+    //Go through each letter of string and store count in hashMap
+    //At the same time store letters in order of appearance in a set
+    //Go through set and whichever occurance is 1 return b/c that is 1st nonrecuring char
+    
+    
+}
