91a86d161d6e124e7c12a46ad7ccc5bdaf938cf2
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-15 15:31:26 -0700

    maxSubarray using kadane algo

--- OnlineVideo/maxSubarray.js
+++ OnlineVideo/maxSubarray.js
@@ -2,31 +2,45 @@
 Given an array find maxium continous subarray
 */
 
+// function maxSubarray (array) {
+
+//     let maxSum = array[0];
+//     let startIndex = 0;
+//     let endIndex = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         let currentSum = array[i];
+
+//         if(currentSum > maxSum) {
+//             maxSum = currentSum;
+//             startIndex = i;
+//             endIndex = i;
+//         }
+//         for (let x= i + 1 ; x < array.length; x++) {
+//             currentSum += array[x];
+
+//             if (currentSum > maxSum) {
+//                 maxSum = currentSum;
+//                 endIndex = x;
+//             }
+//         }
+//     }
+//     console.log(array.slice(startIndex, endIndex+1));
+//     return maxSum;
+// }
+
+//Using kadane algorightm
 function maxSubarray (array) {
+    let maxSum, maxCurrent = array[0];
 
-    let maxSum = array[0];
-    let startIndex = 0;
-    let endIndex = 0;
-    for (let i = 0; i < array.length; i++) {
-        let currentSum = array[i];
+    for (let i = 1; i < array.length; i++) {
+        maxCurrent = Math.max(array[i], maxCurrent + array[i]);
 
-        if(currentSum > maxSum) {
-            maxSum = currentSum;
-            startIndex = i;
-            endIndex = i;
-        }
-        for (let x= i + 1 ; x < array.length; x++) {
-            currentSum += array[x];
-
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-                endIndex = x;
-            }
+        if (maxCurrent > maxSum) {
+            maxSum = maxCurrent;
         }
     }
-    console.log(array.slice(startIndex, endIndex+1));
-    return maxSum;
+    return maxCurrent;
 }
-
 let array = [1,-3,2,1,-1];
 console.log(maxSubarray(array));
+
