a7b246ef90d27f1c4f1dbbeeafb0b2a1b5e976bc
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 20:42:39 -0700

    MVP longestConsecutive letter

--- OnlineVideo/longestConsecutiveLetter.js
+++ OnlineVideo/longestConsecutiveLetter.js
@@ -3,5 +3,25 @@ Given a string return the longest consecutive letter
 */
 
 function longestConsecutiveLetter(string) {
-    
+    //Go through each letter and find longest consecutive letter
+    let maxCount = Number.MIN_SAFE_INTEGER;
+    let maxLetter = '';
+    let currentLetter = string[0];
+    let currentCount = 1;
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i];
+        
+        if (currentLetter === letter) {
+            currentCount++;
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+                maxLetter = letter;
+            }
+        } else {
+            currentLetter = letter;
+            currentCount = 1;
+        }
+
+    }
+    return maxLetter + ' : '+ maxCount;
 }
