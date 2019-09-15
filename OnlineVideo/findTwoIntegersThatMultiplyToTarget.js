b6334be8a642342679edeb28aa09a1e5eddc219a
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 20:57:01 -0700

    Pseudocode for mulTotarget problem

--- OnlineVideo/findTwoIntegersThatMultiplyToTarget.js
+++ OnlineVideo/findTwoIntegersThatMultiplyToTarget.js
@@ -2,5 +2,9 @@
 Given an array on integers find the two integer that multiply to target
 */
 function multToTarget(array, target) {
-    
+    //Go through array once and check if you can find pair to multiply to target
+    //To speed up store process store numbers that is divisible by target
+    //Then go through each and see if divisor can be found
+
+
 }
