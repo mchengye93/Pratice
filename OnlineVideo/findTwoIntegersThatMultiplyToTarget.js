5087f7621beccf9bdb900b829b1df36d71e3f10c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 21:09:01 -0700

    More efficient solution

--- OnlineVideo/findTwoIntegersThatMultiplyToTarget.js
+++ OnlineVideo/findTwoIntegersThatMultiplyToTarget.js
@@ -8,23 +8,20 @@ function multToTarget(array, target) {
     let divisibleNums = {};
     for (let i = 0; i < array.length; i++) {
         let num = array[i];
+
         if ( (target% num)  === 0) {
             divisibleNums[num] = num;
+            let multiple = target/num;
+            
+            if(divisibleNums[multiple]) {
+                return [num, multiple];
+            }
         }
+        
     }
-    //check to see if you can give the other divisor
-    let pairs = [];
-    for (let key in divisibleNums) {
-        let multiple = divisibleNums[key];
-        let multNum = target/multiple;
-
-         //if found return pair
-        if (divisibleNums[multNum]) {
-            pairs.push([divisibleNums[key], multNum]);
-        }
-    }
-    return pairs;
+   
+    return -1;
 
 }
 let arr = [1,7,8,9,4,3,10,5];
-console.log(multToTarget(arr,20)); // [[4,5] , [5,4]];
+console.log(multToTarget(arr,20)); // [4,5] ;
