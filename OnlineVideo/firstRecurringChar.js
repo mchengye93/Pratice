0ccf372968c4b3baf8802b4546012e35edbeb799
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-14 21:16:38 -0700

    Pseduocode firstRecurringChar problem

--- OnlineVideo/firstRecurringChar.js
+++ OnlineVideo/firstRecurringChar.js
@@ -3,5 +3,6 @@ Given a string return the first recurring character
 */
 
 function firstRecurringChar(string) {
-    
+    //Go through string and save onto a hashMap
+    //If already exist in hashMap return the character
 }
