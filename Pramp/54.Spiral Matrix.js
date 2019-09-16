dea0c3c435aac47a4fe2ee0525b8432be17a07f3
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-15 18:21:40 -0700

    MVP spiral question

--- Pramp/54.Spiral Matrix.js
+++ Pramp/54.Spiral Matrix.js
@@ -26,63 +26,54 @@ Output: [1,2,3,4,8,12,11,10,9,5,6,7]
  * @param {number[][]} matrix
  * @return {number[]}
  */
-function spiralCopy(inputMatrix) {
-  //clockwise
-  //row 0 ..end - end-bottom, bottom-left, left-up-end-bottom
-  
-  let spiralNums = [];
-  let row = 0;
-  let col = 0;
- 
-  let rowMax = inputMatrix.length;
-  let colMax = inputMatrix.length;
-  let rowMin = 0;
-  let colMin = 0;
-  //go right row .. colum 0-length-1  add one to rows end
-  
-  //go buttom row...end colum length-1  subtract end of colum-1
-  
-  //go left  end..row .. end to 0 add one to colum = colum+1;
-  
-  //go up   endrow- 2ndrow stays at end of row subtract one to columns end
-  
-  // row  col... colMax
-  while (row < rowMax && col < colMax) {
-      //go right adds 1 to rowMin
-      spiralNums.push(inputMatrix[rowMin][col]);
-      col++;
-      row= 1;
+var spiralOrder = function(matrix) {
+    let result = [];
     
-  }
-  
-  // colMax row...rowMax
-
-  while (row < rowMax) {
-    //down subtracts 1 to maxCol
     
-     spiralNums.push(inputMatrix[row][colMax]);
-     row++;
-     rowMin +=1;
+    let topRow = 0;
+    let bottomRow = matrix.length-1;
+    let leftCol = 0;
+    let rightCol = matrix[0].length-1;
+    
+    
+    
+    let count = 0;
+    
+    while(count <  (matrix.length* matrix[0].length)) {
+        //go right
         
+        for (let i =leftCol; i <= rightCol; i++) {
+            result.push(matrix[topRow][i]);
+            count++;
+            
+        }
+        topRow++;
+  
+        
+        //go bottom
+        for (let i = topRow; i <= bottomRow; i++) {
+            result.push(matrix[i][rightCol]);
+            count++;
+        }
+        rightCol--;
+       
+        
+        //go left
+        for (let i =rightCol; i >= leftCol; i--) {
+            result.push(matrix[bottomRow][i]);
+            count++;
+        }
+        bottomRow--;
+        
+        //go up
+        for (let i = bottomRow; i >= topRow; i--) {
+            result.push(matrix[i][leftCol]);
+            count++;
+            
+        }
+        leftCol++;
+         
+    }
+    return result;
     
-  }
-  
-  // rowMax colMax...col
-  while (col >= 0) {
-    //adds 1 to rowMin
-    spiralNums.push(inputMatrix[rowMax][colMax]);
-    col--;
-    rowMax -=1;
-  }
-  
-  // col rowMax...row
-  while (row >= rowMin){
-    //add 1 to colMin
-    spiralNums.push(inputMatrix[rowMax]col);
-    row--;
-    colMax -=1;
-  }
-
-      
-    
-  
+};
