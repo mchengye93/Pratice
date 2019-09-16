4c9df12a922e2667f2ba87597aa11fcd84a16339
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-15 19:38:30 -0700

    Fix spiral code problem

--- Pramp/54.Spiral Matrix.js
+++ Pramp/54.Spiral Matrix.js
@@ -21,59 +21,69 @@ Input:
 ]
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 */
-
 /**
  * @param {number[][]} matrix
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
     let result = [];
-    
-    
-    let topRow = 0;
-    let bottomRow = matrix.length-1;
-    let leftCol = 0;
-    let rightCol = matrix[0].length-1;
-    
-    
-    
-    let count = 0;
-    
-    while(count <  (matrix.length* matrix[0].length)) {
-        //go right
-        
-        for (let i =leftCol; i <= rightCol; i++) {
-            result.push(matrix[topRow][i]);
-            count++;
-            
-        }
-        topRow++;
-  
-        
-        //go bottom
-        for (let i = topRow; i <= bottomRow; i++) {
-            result.push(matrix[i][rightCol]);
-            count++;
-        }
-        rightCol--;
+   
+   if(matrix[0] === undefined) {
+       return result;
+   }
+   
+   let topRow = 0;
+   let bottomRow = matrix.length-1;
+   let leftCol = 0;
+   let rightCol = matrix[0].length-1;
+   
+   
+   
+   let count = 0;
+   
+   while(topRow <=bottomRow && leftCol <= rightCol) {
+       //go right
        
-        
-        //go left
-        for (let i =rightCol; i >= leftCol; i--) {
-            result.push(matrix[bottomRow][i]);
-            count++;
-        }
-        bottomRow--;
-        
-        //go up
-        for (let i = bottomRow; i >= topRow; i--) {
-            result.push(matrix[i][leftCol]);
-            count++;
-            
-        }
-        leftCol++;
+       for (let i =leftCol; i <= rightCol; i++) {
+           result.push(matrix[topRow][i]);
+           count++;
+           
+       }
+       topRow++;
+ 
+       
+       //go bottom
+       for (let i = topRow; i <= bottomRow; i++) {
+           result.push(matrix[i][rightCol]);
+           count++;
+       }
+       rightCol--;
+      
+       
+       //go left
+       if (topRow <= bottomRow) {
+            for (let i =rightCol; i >= leftCol; i--) {
+             result.push(matrix[bottomRow][i]);
+             count++;
+         }
+         bottomRow--;
+       
          
-    }
-    return result;
-    
+       }
+      
+       //go up
+      if (leftCol <= rightCol) {
+         for (let i = bottomRow; i >= topRow; i--) {
+           result.push(matrix[i][leftCol]);
+           count++;
+           
+       }
+       leftCol++;
+        
+      }
+      
+        
+   }
+   return result;
+   
 };
