eb3b1b081b31a41417b224cbc65d1e066fb21169
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-16 19:08:22 -0700

    busiestTime question

---
+++ Pramp/busiestTimeInMall.js
@@ -0,0 +1,15 @@
+/*
+Given data on time, amount of people, and people going in and out of mall. Return busiest time.
+input:  data = [ [1487799425, 14, 1], 
+                 [1487799425, 4,  0],
+                 [1487799425, 2,  0],
+                 [1487800378, 10, 1],
+                 [1487801478, 18, 0],
+                 [1487801478, 18, 1],
+                 [1487901013, 1,  0],
+                 [1487901211, 7,  1],
+                 [1487901211, 7,  0] ]
+
+output: 1487800378 # since the increase in the number of people
+                   # in the mall is the highest at that point
+*/
