025e9225a9edfe391b1d8dc4cfc6c138ad7cf78e
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-16 19:08:35 -0700

    MVP busiestTime

--- Pramp/busiestTimeInMall.js
+++ Pramp/busiestTimeInMall.js
@@ -13,3 +13,43 @@ input:  data = [ [1487799425, 14, 1],
 output: 1487800378 # since the increase in the number of people
                    # in the mall is the highest at that point
 */
+function findBusiestPeriod(data) {
+    // your code goes here
+    let busiestTime = 0;
+    let maxNum = 0;
+    let currentPeople = 0;
+    
+    let timePeople = {};
+    let arrTime = [];
+    
+    for (let i = 0; i < data.length; i++) {
+      let dataPoint = data[i];
+      let time = dataPoint[0];
+      let amount = dataPoint[1];
+      let inOut = dataPoint[2];
+      
+      if(timePeople[time] === undefined) {
+        timePeople[time] = 0;
+        arrTime.push(time);
+      }
+      
+      if (inOut) {
+        timePeople[time] += amount;
+        
+      } else {
+        timePeople[time] -= amount;
+      }
+    }
+    
+    for (let i = 0; i < arrTime.length; i++) {
+      let key = arrTime[i];
+      currentPeople += timePeople[key];
+      
+      if (currentPeople > maxNum) {
+        maxNum = currentPeople;
+        busiestTime = key;
+      }
+    }
+    
+    return busiestTime;
+  }
