f08ccee3b3e173c971bbf4eed8f7678249631bc6
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-17 19:06:07 -0700

    quadruplet question MVP

--- Pramp/arrayQuadruplet.js
+++ Pramp/arrayQuadruplet.js
@@ -13,3 +13,42 @@ output: [0, 4, 7, 9] # The ordered quadruplet of (7, 4, 0, 9)
                      # asked to return the just one quadruplet (in an
                      # ascending order)
 */
+function findArrayQuadruplet(arr, s) {
+    //arr sorted
+    // look at 4 elements at a time
+    // if they add up to s return the quadruplets
+        //if the sum passes s , stop and return []
+    arr.sort((a,b)=> b-a);
+    
+    //[2, 7, 4, 0, 9, 5, 1, 3]
+    //[0, 1 ,2, 3, 4, 5 ,7, 9] //20
+    
+    //I will start with the smallest num
+    for (let i = arr.length-1; i >=0 ; i--) {
+      let num1 = arr[i];
+      for (let x = i-1; x >=0; x--) {
+        let num2 = arr[x];
+        if (num1 + num2 > s) break;
+    
+        for (let j = x -1 ; j >=0; j--) {
+          let num3 = arr[j];
+          if ((num1 + num2 + num3) > s) break;
+          for (let z = j-1; z >=0 ;z--) {
+            let num4 = arr[z];
+            let sum = num4+num3+num2+num1;
+            if (sum === s) {
+              return [num4,num3,num2,num1].sort();
+            }
+          }
+        }
+        
+      }
+    }
+    return [];
+    
+  }
+  let s = 20;
+  let arr = [0, 1 ,2, 3, 4, 5 ,7, 9];
+  
+  console.log(findArrayQuadruplet(arr,s));
+  
