8e5a5b1d0c8b3efcb4e6eb5a72766bcf7962957d
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-19 13:37:49 -0700

    grantsBudget problem attempt

--- Pramp/findAwardBudgetCap.js
+++ Pramp/findAwardBudgetCap.js
@@ -7,3 +7,32 @@ output: 47 # and given this cap the new grants array would be
            # [2, 47, 47, 47, 47]. Notice that the sum of the
            # new grants is indeed 190
 */
+function findGrantsCap(grantsArray, newBudget) {
+    // your code goes here
+    
+    
+    //find average cap for given grants
+    let average = newBudget/grantsArray.length;
+    let totalAvgLess = 0;
+    let totalCountLess = 0;
+    
+    for (let i = 0; i < grantsArray.length; i++) {
+     //then find those that are less than the average
+    //count and add those that are less than average 
+      let budget = grantsArray[i];
+      if (budget < average) {
+        totalAvgLess += budget;
+        totalCountLess++;
+      }
+    
+    }
+   
+    //subtract to to total budget and then divide amongs greater granta
+    let cap = (newBudget-totalAvgLess)/ (grantsArray.length-totalCountLess); 
+    
+    //check if newCap has value less than it
+    
+    return cap;
+    
+    
+  } 
