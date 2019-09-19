984fb924b00deea571a70753e23aea8675b660a6
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-19 13:37:17 -0700

    grants budget problem

---
+++ Pramp/findAwardBudgetCap.js
@@ -0,0 +1,9 @@
+/*
+Given grantsArray, return a new cap that will minimize grants affected
+
+input:  grantsArray = [2, 100, 50, 120, 1000], newBudget = 190
+
+output: 47 # and given this cap the new grants array would be
+           # [2, 47, 47, 47, 47]. Notice that the sum of the
+           # new grants is indeed 190
+*/
