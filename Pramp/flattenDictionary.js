c4ab31a8677f7c01f5a7feecbd4c50f4d4667066
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-19 23:19:57 -0700

    flatten dictonary MVP

--- Pramp/flattenDictionary.js
+++ Pramp/flattenDictionary.js
@@ -22,3 +22,65 @@ output: {
             "Key2.c.e" : "1"
         }
 */
+
+function flattenDictionary(dict) {
+    let newDict = {};
+    
+    for (let key in dict) {
+      
+      let val = dict[key];
+      //console.log(typeof(val));
+      if ( typeof(val) === 'object' ) {
+        flattenObject(newDict, key, val);
+           
+      } else {
+        newDict[key]=val;
+      }
+    }
+    // go through keys
+        //if key contains an object 
+    return newDict;
+  }
+  
+  function flattenObject(dict,prevKey,object) {
+    //return a new key and value
+  
+    for (let key in object) {
+      let val = object[key];
+     
+       if (typeof(val) === 'object') {
+         if (key.length > 0){
+           flattenObject(dict,prevKey+'.'+key,val);
+         } else {
+           flattenObject(dict,prevKey,val);
+         }
+  
+       } else {
+         if (key.length > 0) {
+           dict[prevKey+'.'+key] = val;
+         } else {
+           dict[prevKey] = val;
+         }
+         
+       }
+    }
+    return;
+    
+  }
+  
+  let dict = {
+              "Key1" : "1",
+              "Key2" : {
+                  "a" : "2",
+                  "b" : "3",
+                  "c" : {
+                      "d" : "3",
+                      "e" : {
+                          "" : "1"
+                      }
+                  }
+              }
+          };
+  console.log(flattenDictionary(dict));
+  
+  
