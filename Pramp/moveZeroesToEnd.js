48d75775b06ad9374d01135b125d78d2db26da5d
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-20 13:01:19 -0700

    MVP zeroesToEnd

--- Pramp/moveZeroesToEnd.js
+++ Pramp/moveZeroesToEnd.js
@@ -2,3 +2,32 @@
 Given a static-sized array of integers arr, move all zeroes in the array to the end 
 of the array. You should preserve the relative order of items in the array.
 */
+
+function moveZerosToEnd(arr) {
+	/**
+	@param arr: integer[]
+	@return: integer[]
+	*/
+
+  let result = [];
+  let zeroCount = 0;
+  
+  //O(n+m) space: O(n)
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeroCount++;
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  
+  
+  for (let i = 0; i < zeroCount; i++ ) {
+    result.push(0);
+  }
+  
+  return result;
+  
+}
+
