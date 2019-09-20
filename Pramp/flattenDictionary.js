4acea84a5551aec377336479b0996ad74413da38
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-19 23:19:33 -0700

    flatten Dictonary prompt

---
+++ Pramp/flattenDictionary.js
@@ -0,0 +1,24 @@
+/*
+Given a dictonary flatten it to reproduce the following result:
+input:  dict = {
+            "Key1" : "1",
+            "Key2" : {
+                "a" : "2",
+                "b" : "3",
+                "c" : {
+                    "d" : "3",
+                    "e" : {
+                        "" : "1"
+                    }
+                }
+            }
+        }
+
+output: {
+            "Key1" : "1",
+            "Key2.a" : "2",
+            "Key2.b" : "3",
+            "Key2.c.d" : "3",
+            "Key2.c.e" : "1"
+        }
+*/
