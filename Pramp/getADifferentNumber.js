31e9c68e39bb1fb62a662e23c21b03b04160f0f7
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-19 23:25:10 -0700

    MVP get smallest positive number

--- Pramp/getADifferentNumber.js
+++ Pramp/getADifferentNumber.js
@@ -4,3 +4,27 @@ input:  arr = [0, 1, 2, 3]
 
 output: 4 
 */
+function getDifferentNumber(arr) {
+    // your code goes here
+    
+    //Go through array and store numbers into hashMap
+    
+    let numMap = {};
+    
+    let maxNum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+      let num = arr[i];
+      numMap[num] = 1;
+      
+      if (num > maxNum) {
+        maxNum = num;
+      }
+    }
+    
+    for (let i = 0; i <= arr.length; i++) {
+      if (numMap[i] === undefined ) {
+        return i;
+      }
+    }
+  }
