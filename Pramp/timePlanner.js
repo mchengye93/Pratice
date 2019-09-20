e355c19bc8d4652ba035d59a128093ffd89cd255
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-19 23:18:06 -0700

    Timeplanner MVP

--- Pramp/timePlanner.js
+++ Pramp/timePlanner.js
@@ -12,3 +12,37 @@ input:  slotsA = [[10, 50], [60, 120], [140, 210]]
         dur = 12
 output: []
 */
+
+function meetingPlanner(slotsA, slotsB, dur) {
+    // find time where both are available at the same and has duration of dur
+    
+  
+    //Go through slots A and slots B and see where overlap
+    for (let i = 0; i < slotsA.length; i++) {
+      let startA = slotsA[i][0];
+      let endA = slotsA[i][1];
+      
+      
+     //find values between slotA start and end
+    // comparing slot B end time
+      for (let x = 0; x < slotsB.length; x++ ){
+        let startB = slotsB[x][0];
+        let endB = slotsB[x][1];
+        
+        
+        let start = Math.max(startA, startB);
+        let end = Math.min(endA, endB);
+        let duration = end-start;
+        
+        if (duration >= dur) {
+            return [start,start+dur];
+        }
+        
+      }
+      
+     
+    }
+    return [];
+     
+  }
+  
