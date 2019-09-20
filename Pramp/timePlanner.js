214505c73c93943cf906810b200ccc0bdb0c7d55
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-19 23:17:54 -0700

    Timeplanner problem

---
+++ Pramp/timePlanner.js
@@ -0,0 +1,14 @@
+/*
+Give available time slots of A and slots of B find the earlist available 
+time for both to meet.
+
+input:  slotsA = [[10, 50], [60, 120], [140, 210]]
+        slotsB = [[0, 15], [60, 70]]
+        dur = 8
+output: [60, 68]
+
+input:  slotsA = [[10, 50], [60, 120], [140, 210]]
+        slotsB = [[0, 15], [60, 70]]
+        dur = 12
+output: []
+*/
