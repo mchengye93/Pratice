49bfc45d3c7f1b46e151cfc105db233076e0f618
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-20 21:45:40 -0700

    meeting planner improved solution

--- Pramp/timePlanner.js
+++ Pramp/timePlanner.js
@@ -14,10 +14,42 @@ output: []
 */
 
 function meetingPlanner(slotsA, slotsB, dur) {
-    // find time where both are available at the same and has duration of dur
+
+    let ia = 0;
+    let ib = 0;
     
-  
+    
+    //Time complexity O(N + M) space complexity O(1)
+    while (ia < slotsA.length && ib < slotsB.length) {
+      
+      //get max of both start time
+      let start = Math.max(slotsA[ia][0], slotsB[ib][0]);
+      
+      //get min of both start time
+      let end = Math.min(slotsA[ia][1], slotsB[ib][1]);
+      
+      
+      if (start + dur <= end) {
+        return [start, start+dur];
+      }
+      
+      // if end time of slotA is less than end timne of B move to next A time
+      if (slotsA[ia][1] < slotsB[ib][1]) {
+        ia++;
+      } else {
+        //if a end time is greater than b start time move B
+        ib++;
+      }
+    }
+    return [];
+
+
+
+
+    // find time where both are available at the same and has duration of dur
     //Go through slots A and slots B and see where overlap
+
+    //Time complexity O(N*M) Space complexity O(1)
     for (let i = 0; i < slotsA.length; i++) {
       let startA = slotsA[i][0];
       let endA = slotsA[i][1];
