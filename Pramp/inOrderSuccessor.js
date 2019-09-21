5da69d13f4e3737f3174a60b80350ca4a3d9786c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-20 23:10:25 -0700

    inordersuccessor problem

--- Pramp/inOrderSuccessor.js
+++ Pramp/inOrderSuccessor.js
@@ -1,3 +1,11 @@
+/*
+In a Binary Search Tree (BST), an Inorder Successor of a node is defined as the node 
+with the smallest key greater than the key of the input node (see examples below). 
+Given a node inputNode in a BST, you’re asked to write a function findInOrderSuccessor
+ that returns the Inorder Successor of inputNode. If inputNode has no Inorder 
+ Successor, return null.
+*/
+
 BinarySearchTree.prototype.findInOrderSuccessor = function(inputNode) {
     // your code goes here
   
