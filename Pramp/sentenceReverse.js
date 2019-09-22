25004f32cdf0b03983387bc63b619468d8e8a867
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-21 21:11:39 -0700

    Implement reverse function

--- Pramp/sentenceReverse.js
+++ Pramp/sentenceReverse.js
@@ -17,7 +17,6 @@ output: [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', '  ',
           'm', 'a', 'k', 'e', 's', '  ',
           'p', 'e', 'r', 'f', 'e', 'c', 't' ]
 */
-
 function reverseWords(arr) {
     /*
     Input: Array of letters where words are seperate with space
@@ -26,47 +25,10 @@ function reverseWords(arr) {
     
     Steps:
       Find the words - start index and end index
-      Naive:
-       Find the words - start index and end index O(N)
-       Save into an array
-       Take that array an read it reversely O(N)
-       -Slice and push into a new array O(N)
+      
     */
-    let arrIdx = [];
-    let reverseArr = [];
-    
-    let start = 0;
-    let end = 0;
-    for (let i=0; i < arr.length; i++ ) {
-      if (arr[i] === ' ' || arr[i].length > 1) {
-        end = i;
-        
-        arrIdx.push([start,end]);
-        
-        arrIdx.push([start,i+1]);
-        start = i+1;
-        
-        
-      }
-      if (i === arr.length-1) {
-        end = i+1;
-        arrIdx.push([start,end]);
-      }
-    }
-    
-    for (let i = arrIdx.length-1; i >= 0; i--) {
-      let start = arrIdx[i][0];
-      let end = arrIdx[i][1];
-      
-      reverseArr.concat(arr.slice(start,end));
-      if (i !== 0) {
-        reverseArr.push(' '); //this  is cat, "  "
-      }
-      
-    }
-    return reverseArr;
-    
-    
+   
+   
    /*
    -Reverse arr
    -Find words reverse the word
@@ -84,58 +46,16 @@ function reverseWords(arr) {
       start++;
       end--;
     }
+    
     //reverse words 
     
-    
-    /* arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',' ',
-                  'm', 'a', 'k', 'e', 's', '  ',
-                  'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
-                  
-        arr =['  ',' ']
-        
-        
-        -this  is cat
-        ->tac si  siht
-          1 3     
-        ->cat is  this 
-        O(1) O(N)
-                */
-    
-   /*
-   function reverseWords(arr):
-      # reverse all characters:
-      n = arr.length
-      mirrorReverse(arr, 0, n-1)
-  
-      # reverse each word:
-      wordStart = null
-      for i from 0 to n-1:
-          if (arr[i] == ' '):
-              if (wordStart != null):
-                  mirrorReverse(arr, wordStart, i-1)
-                  wordStart = null
-          else if (i == n-1):
-              if (wordStart != null):
-                  mirrorReverse(arr, wordStart, i)
-          else:
-              if (wordStart == null):
-                  wordStart = i
-  
-      return arr
-  
-  
-  # helper function - reverses the order of items in arr
-  # please note that this is language dependent:
-  # if are arrays sent by value, reversing should be done in place
-  
-  function mirrorReverse(arr, start, end):
-      tmp = null
-      while (start < end):
-          tmp = arr[start]
-          arr[start] = arr[end]
-          arr[end] = tmp
-          start++
-          end--
-          */
-    
+  }
+  function reverse(arr,start,end) {
+    while (start < end) {
+      let temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }
   }
