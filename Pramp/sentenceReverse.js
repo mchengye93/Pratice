cef8d88b75f86f2f7452f1e79dbf3ada279c7945
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-21 21:06:52 -0700

    Sentence reverse better solution attempt

--- Pramp/sentenceReverse.js
+++ Pramp/sentenceReverse.js
@@ -65,4 +65,77 @@ function reverseWords(arr) {
       
     }
     return reverseArr;
-}
+    
+    
+   /*
+   -Reverse arr
+   -Find words reverse the word
+       -Ignore spaces
+    
+   */
+    let start = 0;
+    let end = arr.length-1;
+    while (start != end) {
+      let endLetter = arr[end];
+      let startLetter = arr[start];
+      
+      arr[start] = endLetter;
+      arr[end] = startLetter;
+      start++;
+      end--;
+    }
+    //reverse words 
+    
+    
+    /* arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',' ',
+                  'm', 'a', 'k', 'e', 's', '  ',
+                  'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
+                  
+        arr =['  ',' ']
+        
+        
+        -this  is cat
+        ->tac si  siht
+          1 3     
+        ->cat is  this 
+        O(1) O(N)
+                */
+    
+   /*
+   function reverseWords(arr):
+      # reverse all characters:
+      n = arr.length
+      mirrorReverse(arr, 0, n-1)
+  
+      # reverse each word:
+      wordStart = null
+      for i from 0 to n-1:
+          if (arr[i] == ' '):
+              if (wordStart != null):
+                  mirrorReverse(arr, wordStart, i-1)
+                  wordStart = null
+          else if (i == n-1):
+              if (wordStart != null):
+                  mirrorReverse(arr, wordStart, i)
+          else:
+              if (wordStart == null):
+                  wordStart = i
+  
+      return arr
+  
+  
+  # helper function - reverses the order of items in arr
+  # please note that this is language dependent:
+  # if are arrays sent by value, reversing should be done in place
+  
+  function mirrorReverse(arr, start, end):
+      tmp = null
+      while (start < end):
+          tmp = arr[start]
+          arr[start] = arr[end]
+          arr[end] = tmp
+          start++
+          end--
+          */
+    
+  }
