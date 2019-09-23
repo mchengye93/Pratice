0ba6c27cda77553646216bea6b5808a577f89515
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-22 21:22:10 -0700

    Finding roots problem

---
+++ Pramp/findRoot.js
@@ -0,0 +1,9 @@
+/*
+Given x and n find the root of given values. It must satisfy following condition |y-root(x,n)| < 0.001).
+Examples:
+input:  x = 7, n = 3
+output: 1.913
+
+input:  x = 9, n = 2
+output: 3
+*/
