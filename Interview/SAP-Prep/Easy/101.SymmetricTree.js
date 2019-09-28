bdbe9acddac57bb7fa40d0351dcd531d3e0c6584
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-27 21:30:05 -0700

    MVP Symmetric tree

--- Interview/SAP-Prep/Easy/101.SymmetricTree.js
+++ Interview/SAP-Prep/Easy/101.SymmetricTree.js
@@ -18,3 +18,27 @@ But the following [1,2,2,null,3,null,3] is not:
    \   \
    3    3
 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    return isMirror(root,root);
+};
+
+var isMirror = (tree1, tree2) => {
+    if (tree1 === null && tree2=== null) {
+        return true;
+    }
+    if (tree1 === null || tree2 === null) {
+        return false;
+    }
+    return (tree1.val === tree2.val) && isMirror(tree1.right, tree2.left) && isMirror(tree1.left, tree2.right);
+}
