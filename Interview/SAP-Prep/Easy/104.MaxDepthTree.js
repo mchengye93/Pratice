1e2ba88bafc11535718424c74de52b4d59b673e1
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-28 14:27:51 -0700

    MaxDept recursive solution

--- Interview/SAP-Prep/Easy/104.MaxDepthTree.js
+++ Interview/SAP-Prep/Easy/104.MaxDepthTree.js
@@ -24,20 +24,22 @@ return its depth = 3.
  * }
  */
 /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
  * @param {TreeNode} root
  * @return {number}
  */
 var maxDepth = function(root) {
-    
-    
     if (root === null) {
         return 0;
-    } else {
-        let left = maxDepth(root.left);
-        let right = maxDepth(root.right);
-        
-        return Math.max(left,right) + 1;
     }
+    return Math.max(maxDepth(root.left) + 1, maxDepth(root.right)+1);
+    
  
     
 };
