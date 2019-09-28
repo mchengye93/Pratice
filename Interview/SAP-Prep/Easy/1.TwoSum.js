17138a3df0f6f7ac593b656685e552471d0f183f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-28 14:17:23 -0700

    Two Sum optimal solution

--- Interview/SAP-Prep/Easy/1.TwoSum.js
+++ Interview/SAP-Prep/Easy/1.TwoSum.js
@@ -10,3 +10,25 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+//Time Complexity: O(n) Space Complexity: O(n)
+var twoSum = function(nums, target) {
+    let seenNums = {};
+    
+    for (let i = 0; i < nums.length; i++ ) {
+        let complement = target - nums[i];
+        
+        
+        if (seenNums[complement] !== undefined) {
+            return [seenNums[complement], i];
+        }
+        seenNums[nums[i]] = i;
+        
+    }
+    return -1;
+       
+};
