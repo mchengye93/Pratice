e5af9bc8ddc9a8977a39db0bcea8c4bb5d6f261b
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-27 21:32:38 -0700

    MVP mergeInterval

--- Interview/SAP-Prep/Medium/56.MergeIntervals.js
+++ Interview/SAP-Prep/Medium/56.MergeIntervals.js
@@ -14,3 +14,51 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 NOTE: input types have been changed on April 15, 2019. Please reset to default code definition 
 to get new method signature.
 */
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    //take first interval and find any overlaps to the rest
+    let newResult = [];
+    for (let i = 0; i < intervals.length; i++) {
+        let interval = intervals[i];
+        let lower = interval[0];
+        let upper = interval[1];
+        let change = false;
+        console.log('[currentupper,currentLower]', lower,upper);
+        for (let x = i + 1; x < intervals.length; x++) {
+            let nextInterval = intervals[x];
+            let nextLower = nextInterval[0];
+            let nextUpper = nextInterval[1];
+            
+            console.log('[nextlower,nextupper]', nextLower, nextUpper);
+            //we are at same range so we update upper
+            
+            if (nextLower >= lower && nextLower <= upper) {
+                
+                    if (nextUpper > upper) {
+                        upper = nextUpper;
+                        console.log('Update upper: ', upper );
+                        change = true;
+                        
+                    }
+                    
+                }
+            
+             if (nextUpper >= lower && nextUpper <= upper) {
+                 
+                if (nextLower < lower) {
+                    lower = nextLower;
+                    console.log('Update lower: ', lower);
+                    change= true;
+
+                }
+            }
+        
+            }
+        if (change) { i++};
+         newResult.push([lower,upper]);   
+        }
+    return newResult;
+    }
