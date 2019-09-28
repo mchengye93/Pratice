0212ffc4c3d76f44f4d8b09f31e1b64a5b578236
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-27 21:46:51 -0700

    MVP maxDepth problem

--- Interview/SAP-Prep/Easy/104.MaxDepthTree.js
+++ Interview/SAP-Prep/Easy/104.MaxDepthTree.js
@@ -16,3 +16,28 @@ Given binary tree [3,9,20,null,null,15,7],
    15   7
 return its depth = 3.
 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    
+    
+    if (root === null) {
+        return 0;
+    } else {
+        let left = maxDepth(root.left);
+        let right = maxDepth(root.right);
+        
+        return Math.max(left,right) + 1;
+    }
+ 
+    
+};
