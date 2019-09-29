6d1fb7d788396f4af537f4cfca9fc7974765c60b
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-28 18:05:26 -0700

    Optimized merge intervals

--- Interview/SAP-Prep/Medium/56.MergeIntervals.js
+++ Interview/SAP-Prep/Medium/56.MergeIntervals.js
@@ -19,26 +19,24 @@ to get new method signature.
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if(intervals.length === 0) return [];
-    let res = [intervals[0]];
-    let x = 0;
-
-    for (let i = 1; i < intervals.length; i++){
-        let start = intervals[i][0];
-        let end = intervals[i][1];
-
-        let resCurrent = res[x];
-        let resStart = resCurrent[0];
-        let resEnd = resCurrent[1];
-
-        if (start <= resEnd && end > resEnd) {
-            res[x][1] = end;
+    if(!intervals.length) return intervals;
+    
+    intervals.sort((a,b) => a[0]- b[0]);
+    
+    const res = [intervals[0]];
+    
+    for (let i = 0; i < intervals.length; i++) {
+        const currInterval = intervals[i];
+        const lastInterval = res[res.length-1];
+        
+        if (currInterval[0] <= lastInterval[1]) {
+            lastInterval[1] = Math.max(currInterval[1], lastInterval[1]);
+            
         } else {
-            res.push([start,end]);
-            x++;
+            res.push(currInterval);
         }
-
     }
     return res;
+ 
     }
 
