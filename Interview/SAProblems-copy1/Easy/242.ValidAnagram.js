8cc7496eabbc5cd4aff6b56d9306bf91bae4e9eb
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-02 15:19:54 -0700

    isAnagram MVP

--- Interview/SAProblems-copy1/Easy/242.ValidAnagram.js
+++ Interview/SAProblems-copy1/Easy/242.ValidAnagram.js
@@ -21,6 +21,31 @@ solution to such case?
  * @param {string} t
  * @return {boolean}
  */
+
+ //Time Complexity: O(N) SpaceComplexity: O(N)
 var isAnagram = function(s, t) {
+    if (s.length!== t.length) return false;
+
+    let charCount = {};
+    for (let i = 0; i < s.length; i++) {
+        let letter = s[i];
+
+        if(!charCount[letter]) {
+            charCount[letter] = 1;
+        } else {
+            charCount[letter]++;
+        }
+    }
+
+    for (let i = 0; i < t.length; i++){
+        let letter = t[i];
+        if (!charCount[letter]) {
+            return false;
+        } else {
+            charCount[letter]--;
+            if (charCount[letter] < 0) return false;
+        }
+    }
+    return true;
     
 };
