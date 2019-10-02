318010371a91f3fe14b0686d92b3e2bd3ee5b7b3
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-02 15:51:44 -0700

    MVP maxProfit

--- Interview/SAProblems-copy1/Easy/121.BestTimeBuySellStock.js
+++ Interview/SAProblems-copy1/Easy/121.BestTimeBuySellStock.js
@@ -23,7 +23,22 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
  * @return {number}
  */
 
-
-var maxProfit = function(prices) {
+//TimeComplexity: O(N) Space: O(1)
+ var maxProfit = function(prices) {
+     let minPrice = prices[0];
+     let maxProfit = 0;
+     
+     for (let i = 0 ;i < prices.length ;i++) {
+          if (prices[i] < minPrice) {
+               minPrice = prices[i];
+          } else {
+               let profit = prices[i] - minPrice;
+               if (profit > maxProfit) {
+                    maxProfit = profit;
+                   
+               }
+          }
+     }
+     return maxProfit;
  
 };
