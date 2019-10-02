4ab35b13a777213c0022e88073ea30c9419b2a24
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-02 15:48:08 -0700

    maxDept MVP

--- Interview/SAProblems-copy1/Easy/104.maxDepthBT.js
+++ Interview/SAProblems-copy1/Easy/104.maxDepthBT.js
@@ -30,6 +30,9 @@ return its depth = 3.
  */
 //
 var maxDepth = function(root) {
-  
+    if (root === null) return 0;
+
+    return Math.max(maxDepth(root.left)+1, maxDepth(root.right) + 1);
     
 };
+
