95db7354f6eca98bf011bf82d4e9c7d3b23ff569
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-02 14:59:33 -0700

    twoSum MVP

---
+++ Interview/SAProblems-copy1/Easy/1.TwoSum.js
@@ -0,0 +1,36 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up 
+to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use
+ the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ //Runtime: O(N)  Space: O(N)
+var twoSum = function(nums, target) {
+    let num = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target- nums[i];
+        
+        if (num[complement] !== undefined) {
+            return [num[complement], i];
+        }
+        num[nums[i]] = i;
+    }
+   
+    return [];
+       
+};
