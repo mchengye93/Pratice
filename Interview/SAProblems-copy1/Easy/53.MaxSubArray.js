72e0e97220ddc0e58dfd82951697cbcc44f9727d
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-02 15:02:18 -0700

    MaxSubArray problem

--- Interview/SAProblems-copy1/Easy/53.MaxSubArray.js
+++ Interview/SAProblems-copy1/Easy/53.MaxSubArray.js
@@ -17,6 +17,15 @@ the divide and conquer approach, which is more subtle.
  * @param {number[]} nums
  * @return {number}
  */
+//Runtime: O(N) Space Complexity: O(1)
 var maxSubArray = function(nums) {
+    let max = nums[0];
+    let currentMax = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        currentMax = Math.max(currentMax+nums[i], nums[i]);
+        max = Math.max(max,currentMax);
+    }
+    return max;
     
 };
