c9a8cb180d559347ce7fe54a10df8c37d8862c64
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-02 14:24:22 -0700

    TwoSum problem setup

--- Interview/SAPQ/Easy/1.TwoSum.js
+++ Interview/SAPQ/Easy/1.TwoSum.js
@@ -12,3 +12,12 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  
+       
+};
