86f1d1a9f2c44544187c67fdaf4bef596077665f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-07 21:42:53 -0700

    LongestPalidrome solution

--- Interview/SAProblems-copy1/Medium/5.LongestPalindromeSubstring.js
+++ Interview/SAProblems-copy1/Medium/5.LongestPalindromeSubstring.js
@@ -19,3 +19,26 @@ Output: "bb"
 var longestPalindrome = function(s) {
 
 };
+var longestPalindrome = function(s) {
+    let startIdx = 0;
+   let maxLength = 1;
+
+   function expandAroundMiddle(left,right) {
+       while (left >= 0 && right < s.length && s[left]===s[right]){
+           const currentPalLength = right-left + 1;
+           if (currentPalLength > maxLength) {
+               maxLength = currentPalLength;
+               startIdx = left;
+           }
+           left -= 1;
+           right +=1;
+       }
+   }
+
+   for (let i = 0; i < s.length; i++) {
+       expandAroundMiddle(i-1, i+1);
+       expandAroundMiddle(i, i+1);
+   }
+
+   return s.slice(startIdx, startIdx+ maxLength);
+};
--- Interview/SAProblemsPractice/Medium/5.LongestPalindromeSubstring.js
+++ Interview/SAProblemsPractice/Medium/5.LongestPalindromeSubstring.js
@@ -17,5 +17,31 @@ Output: "bb"
  * @return {string}
  */
 var longestPalindrome = function(s) {
+    let maxLength = 0;
+    let start = 0;
+    for (let i = 0; i <  s.length; i++) {
+        let len1 = expandFromCenter(s,i-1,i+1);
+        let len2 = expandFromCenter(s,i,i+1);
+        if(len1 > maxLength || len2 > maxLength) start = i;
+        maxLength = Math.max(len1, len2);
 
+    }
+    return s.slice(start,maxLength);
+ 
 };
+
+//function to expand from center and see if palindrome and then return length
+
+var expandFromCenter = function(s,left,right) {
+    let length = 0;
+    while(right < s.length && left >=0) {
+        if (s[left] === s[right]) {
+            length = right - left;
+            right++;
+            left--;
+        } else {
+            break;
+        }
+    }
+    return length;
+}
