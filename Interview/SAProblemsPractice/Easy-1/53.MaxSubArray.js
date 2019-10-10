e244ea54bad1fe452540ffde7ef0b3fcd4ecc49a
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-10 16:33:57 -0700

    maxSum problem

--- Interview/SAProblemsPractice/Easy-1/53.MaxSubArray.js
+++ Interview/SAProblemsPractice/Easy-1/53.MaxSubArray.js
@@ -18,5 +18,13 @@ the divide and conquer approach, which is more subtle.
  * @return {number}
  */
 var maxSubArray = function(nums) {
+    let maxSum = nums[0];
+    let currentSum = nums[0];
     
+    for (let i = 1 ; i < nums.length;i++) {
+        currentSum = Math.max(currentSum+ nums[i], nums[i]);
+        maxSum = Math.max(maxSum,currentSum);
+    }
+    
+    return maxSum;
 };
