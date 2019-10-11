835150a71602aaf7a95eb79e7fe4e087fafa1a1c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-10 19:08:24 -0700

    longestPalindrome optimal solution

--- Interview/SAProblemsPractice/Medium-2/5.LongestPalindromeSubstring.js
+++ Interview/SAProblemsPractice/Medium-2/5.LongestPalindromeSubstring.js
@@ -16,6 +16,33 @@ Output: "bb"
  * @param {string} s
  * @return {string}
  */
+
+ //Runtime: O(n^2) Space: O(1)
 var longestPalindrome = function(s) {
+    let start = 0;
+    let maxLength = 1;
+    function expandFromCenter(left,right) {
+        let length = 1;
+        while(left >= 0 && right <= s.length && s[left] === s[right]) {
+            length = right - left + 1;
+            
+        if (length > maxLength) {
+            maxLength = length;
+            start = left;
+            }
+            
+            left--;
+            right++;
+        }
+    
+    }
+    for (let i = 0; i < s.length; i++) {
+        expandFromCenter(i-1,i+1);
+        expandFromCenter(i,i+1);
+
+    }
+    return s.slice(start,start+maxLength);
+
 
 };
+
