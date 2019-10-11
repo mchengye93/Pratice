1af7ed75d84e7ad53ddff77d39ab6a2fd0b3ee38
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-10 18:41:20 -0700

    firstUniqChar optimal solution

--- Interview/SAProblemsPractice/Easy-1/387.FirstUniqChar.js
+++ Interview/SAProblemsPractice/Easy-1/387.FirstUniqChar.js
@@ -15,6 +15,20 @@ Note: You may assume the string contain only lowercase letters.
  * @param {string} s
  * @return {number}
  */
+
+ //Runtime: O(N) Space: O(N)
 var firstUniqChar = function(s) {
+    let charCount = {};
+    for (let i = 0 ; i < s.length; i++) {
+        if (charCount[s[i]] === undefined) {
+            charCount[s[i]] = 1;
+        } else {
+            charCount[s[i]]++;
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if(charCount[s[i]] === 1) return i;
+    }
+    return -1;
     
 };
