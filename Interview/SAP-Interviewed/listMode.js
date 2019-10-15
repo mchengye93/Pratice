d08044d78588ff87ad5c7b9fbe62ef00c8e0af1b
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-14 22:59:40 -0700

    return maxInt count problem

---
+++ Interview/SAP-Interviewed/listMode.js
@@ -0,0 +1,23 @@
+/*
+Given a list of integers return maxCount
+*/
+
+let maxCount = function(arr) {
+    if (arr.length === 0 || arr === null) return -1;
+    let hashMap = {};
+    let maxInt = arr[0];
+    let maxCount = 1;
+
+    for (let i = 0; i < arr.length; i++) {
+        if(hashMap[arr[i]] === undefined) {
+            hashMap[arr[i]] = 1;
+        } else {
+            hashMap[arr[i]]++;
+            if(hashMap[arr[i]] > maxCount) {
+                maxCount = hashMap[arr[i]];
+                maxInt= arr[i];
+            }
+        }
+    }
+    return maxInt;
+}
