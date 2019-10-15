25b05917e9c5ccd4a52f726b4511b740243efeb8
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-14 22:49:14 -0700

    inOrderLevel problem

--- Interview/SAProblemsPractice/Medium-1/102.OrderLevelTraversal.js
+++ Interview/SAProblemsPractice/Medium-1/102.OrderLevelTraversal.js
@@ -29,6 +29,26 @@ return its level order traversal as:
  * @return {number[][]}
  */
 var levelOrder = function(root) {
+  let stack = [];
+  let result = [];
+  stack.push(root);
+
+  while(stack.length > 0) {
+
+    let temp = [];
+    let size = stack.length;
+
+    for (let i = 0 ; i < size; i++) {
+      let node = stack.shift();
+      temp.push(node.val);
+
+      if(node.left) stack.push(node.left);
+      if(node.right)stack.push(node.right);
+    }
+    result.push(temp);
+
+  }
+  return result;
 
 };
 
