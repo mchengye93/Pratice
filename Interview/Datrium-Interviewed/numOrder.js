27de9a228a0cdb7f853f856163bc08a836fe53a4
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-18 13:56:02 -0700

    missing num hashmap approach

--- Interview/Datrium-Interviewed/numOrder.js
+++ Interview/Datrium-Interviewed/numOrder.js
@@ -2,3 +2,23 @@
 Given an array of unorder numbers find the missing number in the array
 */
 
+let missingNum = (arr) => {
+
+}
+
+/* HashMap approach: Time Complexity: O(n) Space: O(n) */
+
+
+let missingNum = (arr) => {
+
+    let nums = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        if(nums[arr[i]] === undefined) nums[arr[i]] = 1;
+    }
+
+    for (let i = 1; i <=arr.length; i++) {
+        if(!nums[i]) return i;
+    }
+}
+
