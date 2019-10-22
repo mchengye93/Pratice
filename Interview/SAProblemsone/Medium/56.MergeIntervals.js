82f3e480b96da784c9d09baaec70ea943bd1300a
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-21 21:23:22 -0700

    mergeIntervals solution

--- Interview/SAProblemsone/Medium/56.MergeIntervals.js
+++ Interview/SAProblemsone/Medium/56.MergeIntervals.js
@@ -20,8 +20,27 @@ to get new method signature.
  */
 
  //Time Complexity: O(NlogN) used sort, Space complexity: O(1) We sort inplace no extra data structure
-var merge = function(intervals) {
+ var merge = function(intervals) {
+    if (intervals.length === 0 || intervals === null) return [];
+    intervals.sort((a,b)=> a[0]- b[0]);
+    let results = [intervals[0]];
+    let idx  = 0;
 
- 
+    for (let i = 1; i < intervals.length; i++ ) {
+        let currentIntMin = results[idx][0];
+        let currentIntMax = results[idx][1];
+
+        let nextIntMin = intervals[i][0];
+        let nextIntMax = intervals[i][1];
+
+        if (nextIntMin <= currentIntMax) {
+            results[idx][0] = Math.min(currentIntMin,nextIntMin);
+            results[idx][1] = Math.max(currentIntMax, nextIntMax);
+        } else {
+            results.push(intervals[i]);
+            idx++;
+        }
+    }
+    return results;
+
 }
-
