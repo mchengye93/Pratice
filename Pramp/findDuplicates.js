661337f1db607f0ab62c302653c5bb8fc90927b9
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-21 21:14:51 -0700

    findDuplicates problem

--- Pramp/findDuplicates.js
+++ Pramp/findDuplicates.js
@@ -1,3 +1,12 @@
+/*
+Given two sorted arrays arr1 and arr2 of passport numbers, implement a function findDuplicates
+ that returns an array of all passport numbers that are both in arr1 and arr2. Note that the
+  output array should be sorted in an ascending order.
+
+Let N and M be the lengths of arr1 and arr2, respectively. Solve for two cases and analyze the
+ time & space complexities of your solutions: M ≈ N - the array lengths are approximately the
+  same M ≫ N - arr2 is much bigger than arr1.
+*/
 function findDuplicates(arr1, arr2) {
   
     //idx arr1, idxarr2
@@ -20,7 +29,7 @@ function findDuplicates(arr1, arr2) {
     M = 100
     n = 2*/
     
-    
+    /*Runtime: Onlog(m) Space Complexity: O(n) */
     function binarySearch(arr,start,end, target) {
       
       while(start<=end) {
@@ -49,7 +58,7 @@ function findDuplicates(arr1, arr2) {
     return duplicates;
     
    
-    
+    /*Runtime: O(m+n) Space Complexity: O(m+n) */
     /*
     while(idx2 < arr2.length && idx1 < arr1.length) {
       if(arr2[idx2] < arr1[idx1]) {
