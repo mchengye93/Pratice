8ebcfeaf5f453e6cd79df5bd7de837a5e6015c70
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-21 21:38:00 -0700

    inOrderTraversal iterative solution

--- Interview/SAProblemsone/Medium/94.InOrderTraversal.js
+++ Interview/SAProblemsone/Medium/94.InOrderTraversal.js
@@ -26,6 +26,24 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
  */
 
 var inorderTraversal = function(root) {
+   let res = [];
+   let stack = [];
+   while (true) {
+   
+      if(root !== null) {
+         stack.push(root);
+         root = root.left;
+      } else {
+         if (!stack.length) break;
+         root = stack.pop();
+         res.push(root.val);
+         root = root.right;
+      }
+   }
+   return res;
+}
+
+var inorderTraversal = function(root) {
    let result = [];
 
    let inOrder = (root) => {
