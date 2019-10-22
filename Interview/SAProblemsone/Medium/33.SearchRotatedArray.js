014c6129bf717053562cb9bb73ca8bc14aa9dca6
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-21 19:35:50 -0700

    MVP rotateArray problem

--- Interview/SAProblemsone/Medium/33.SearchRotatedArray.js
+++ Interview/SAProblemsone/Medium/33.SearchRotatedArray.js
@@ -22,7 +22,27 @@ Output: -1
 */
 
 function search(nums, target) {    
-    
+    let inflectionIdx = inflectionPoint(nums);
+    let left = binarySearch(nums, 0, inflectionIdx-1, target);
+    let right = binarySearch(nums,inflectionIdx, nums.length-1, target);
+
+    return Math.max(left,right);
+}
+
+let binarySearch = (nums,left,right, target) => {
+
+    while(left <=right) {
+        let mid = Math.floor((left+right)/2); 
+
+        if (nums[mid] === target)return mid; 
+
+        if (nums[mid] > target){
+            right= mid-1;  
+        } else {
+            left = mid + 1;
+        }  
+    }
+    return -1;
 }
 
 let inflectionPoint= (nums) => {
@@ -40,7 +60,8 @@ let inflectionPoint= (nums) => {
 
         if (nums[mid] > nums[mid+1]) {
             return mid+1;
-        } else if (nums[mid] < nums[mid-1]) {
+        } 
+        if (nums[mid] < nums[mid-1]) {
             return mid;
         }
 
@@ -53,4 +74,5 @@ let inflectionPoint= (nums) => {
 }
 
 let nums =[4,5,6,7,0,1,2];
+
 console.log(inflectionPoint(nums)); //4
