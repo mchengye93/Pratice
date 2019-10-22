85e952e8029d9896c351404e4ddd211835959540
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-21 19:48:26 -0700

    lengthOflongestSubstring problem

--- Interview/SAProblemsone/Medium/3.LongestSubstrNoRepeat.js
+++ Interview/SAProblemsone/Medium/3.LongestSubstrNoRepeat.js
@@ -25,5 +25,27 @@ Explanation: The answer is "wke", with the length of 3.
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+    //window
+    let lastPos = {};
+    let maxLength = 0;
+    let currentLength = 0;
+    let startIdx = 0;
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (lastPos[s[i]] >= startIdx) {
+            startIdx =lastPos[s[i]] + 1;     
+        }
+
+        currentLength = i-startIdx+1;
+
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+
+        }
+        lastPos[s[i]] = i;
+    }
+    return maxLength;
+
  
 };
