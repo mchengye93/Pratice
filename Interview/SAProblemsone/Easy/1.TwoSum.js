49d624e1df37a8da08937ca8517f9611e0482820
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-21 21:50:54 -0700

    twoSum solution

--- Interview/SAProblemsone/Easy/1.TwoSum.js
+++ Interview/SAProblemsone/Easy/1.TwoSum.js
@@ -17,7 +17,17 @@ return [0, 1].
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(nums, target) {
-  
+    let numPos = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (numPos[complement] !== undefined) {
+            return [numPos[complement], i];
+        }
+        numPos[nums[i]] = i;
+    }
        
 };
