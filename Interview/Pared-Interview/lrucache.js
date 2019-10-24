ae42530a741db811641acd8930dd553741a19646
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-23 21:57:58 -0700

    LRU cache

--- Interview/Pared-Interview/lrucache.js
+++ Interview/Pared-Interview/lrucache.js
@@ -20,36 +20,73 @@ class Cache {
     }
 
     set(key,value) {
-        //most recently put on head
+        
+        this.checkCapacity();
+        //if head is null then new value is head/tail
+        if(!this.head){
+            this.head = this.tail = new Node(key, value);
+        } else{
+            //else set node to be head 
+            const node = new Node(key, value, this.head);
 
-        //if head is null then set tail and head
+            //oldHead's prev is node
+            this.head.prev = node;
 
-        //else replace head with new node
-
-        //put the key on cache and node {key: node}
-        //add one to size
+            //node becomes head
+            this.head = node;
+        }
+    
+        //Update the cache map
+        this.cache[key] = this.head;
+        this.size++;
 
     }
 
-    get(key) {
-        //check to see if it is in cache
-        //if it isn't then return null
+    read(key) {
+        //if key exist return value
+        if(this.cache[key]){
+            const value = this.cache[key].value;
+            
+            //remove node
+            this.remove(key)
 
-        
-        //if exist make node prev to point to node's next
-        // node next prev point to prev node
-        //take oldHead and set old head prev to be node
-        // node next is oldHead
-        //this.head = node;
+            //put as head
+            // write node again to the head of LinkedList to make it most recently used
+            this.set(key, value);
+      
+            return value;
+          }
+          return null; //item does not exist in cache
        
     }
 
-    delete(key) {
-        //when we pass limit we are remove elements from tail
-        //set tail previous to be tail and 
-        //if it is both head and tail then set them to be null
-        //we also remove key from cache map
-    }
+  
+    remove(key){
+        const node = this.cache[key];
+        
+        //if node prev is not null reset prev next to point node next
+        if(node.prev !== null){
+          node.prev.next = node.next;
+        }else{
+          this.head = node.next;
+        }
+        
+        //if node next is not null set node.next.prev to point node.prev
+        if(node.next !== null){
+          node.next.prev = node.prev;
+        }else{
+            //if next is null then it was tail so set new tail to be current's prev
+          this.tail = node.prev
+        }
+    
+        //remove from cache and lower size
+        delete this.cache[key];
+        this.size--;
+      }
 
-
+      checkCapacity(){
+        if(this.size === this.capacity){
+          this.remove(this.tail.key)
+        }
+      }
 }
