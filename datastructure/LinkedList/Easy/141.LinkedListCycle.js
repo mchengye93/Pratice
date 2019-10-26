8d05e7d43ea2876b43c5ad4e5d3426694e24142d
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-26 14:18:49 -0700

    cycledLinkedList problem

---
+++ datastructure/LinkedList/Easy/141.LinkedListCycle.js
@@ -0,0 +1,27 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents 
+the position (0-indexed) in the linked list where tail connects to. If pos is -1, 
+then there is no cycle in the linked list.
+
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory?
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    
+};
