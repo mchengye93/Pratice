fefe2582a700c7184f23e393582861589f4914e2
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-26 14:19:41 -0700

    intersection of two linkedList

---
+++ datastructure/LinkedList/Easy/160.IntersectionTwoLinkedList.js
@@ -0,0 +1,19 @@
+/*
+Write a program to find the node at which the intersection of two singly
+ linked lists begins.
+*/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+}
