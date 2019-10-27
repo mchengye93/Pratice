05b57a19c2b334519c261b867d6dd5534c172afd
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-27 00:25:58 -0700

    pivot function for rotateArr

--- Interview/SAProblemspratice/Medium/33.SearchRotatedArray.js
+++ Interview/SAProblemspratice/Medium/33.SearchRotatedArray.js
@@ -24,3 +24,26 @@ Output: -1
 function search(nums, target) {    
 
 }
+
+let pivot = (arr) => {
+    let left = 0;
+    let right = arr.length-1;
+
+    if (arr.length === 1) return 0;
+    if (arr[left] < arr[right]) return 0;
+
+
+    while(left <= right) {
+        let mid = Math.floor((left+right)/2);
+
+        if (arr[mid] > arr[mid+1]) return mid+1;
+        if (arr[mid] < arr[mid-1]) return mid;
+
+        if (arr[mid] > arr[left]) {
+            left = mid +1;
+        } else {
+            right = mid-1;
+        }
+
+    }
+}
