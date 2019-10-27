1212c31e7a937fdae5ebf48fcbd560034dac0064
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-27 00:28:31 -0700

    binarySearch function

--- Interview/SAProblemspratice/Medium/33.SearchRotatedArray.js
+++ Interview/SAProblemspratice/Medium/33.SearchRotatedArray.js
@@ -47,3 +47,18 @@ let pivot = (arr) => {
 
     }
 }
+
+let binarySearch = (arr,target,left,right) => {
+    
+    while(left <= right) {
+        let mid = Math.floor((left+right)/2);
+        if (arr[mid]===target) return mid;
+
+        if (arr[mid] < target) {
+            left = mid+1;
+        } else {
+            right = mid-1;
+        }
+    }
+    return -1;
+}
