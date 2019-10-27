56ab4d56129bc9029c59e046e288287f4aec0ed9
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-26 23:56:16 -0700

    2dsearch MVP

--- Interview/SAProblemspratice/Medium/240.2D-Search.js
+++ Interview/SAProblemspratice/Medium/240.2D-Search.js
@@ -26,6 +26,19 @@ Given target = 20, return false.
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
+  let row = 0;
+  let col = matrix[0].length-1;
 
+  while (row < matrix.length && col >=0) {
+    if (matrix[row][col] === target) return true;
+
+    if (matrix[row][col] > target) {
+      col--;
+    } else {
+      row++;
+    }
+
+  }
+  return false;
 
 };
