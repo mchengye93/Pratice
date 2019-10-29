57502117a9a83b040454985a2faf43195e3170fa
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-28 21:55:54 -0700

    maxDepth problem

--- Pratice/SAProblems/Easy/559.MaxDepthN-aryTree.js
+++ Pratice/SAProblems/Easy/559.MaxDepthN-aryTree.js
@@ -32,5 +32,6 @@ The total number of nodes is at most 5000.
  * @return {number}
  */
 var maxDepth = function(root) {
-    
+    if (!root) return 0;
+    return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
 };
