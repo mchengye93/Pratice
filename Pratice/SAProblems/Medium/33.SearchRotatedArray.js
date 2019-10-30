509da154eb2c35782d839f65ef056b412d7538c9
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-29 19:33:32 -0700

    find rotation pivot

--- Pratice/SAProblems/Medium/33.SearchRotatedArray.js
+++ Pratice/SAProblems/Medium/33.SearchRotatedArray.js
@@ -24,3 +24,27 @@ Output: -1
 function search(nums, target) {    
 
 }
+let pivot = (nums) => {
+    let left = 0;
+    let right = nums.length-1;
+
+    if(nums.length === 1) return 0;
+    if (nums[left] < nums[right]) return 0;
+
+    while(left <= right) {
+        let mid = Math.floor((left+right)/2);
+
+        if (nums[mid] > nums[mid+1]) return mid+1;
+        if (nums[mid] < nums[mid-1]) return mid;
+
+        if (nums[left] < nums[mid]) {
+            left = mid + 1;
+        } else {
+            right = mid-1;
+        }
+    }
+}
+
+let binarySearch = (nums,left,right) => {
+    
+}
